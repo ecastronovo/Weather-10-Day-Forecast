7b6e5c8ec8068bc4306c9b9d3a78d74d09dcabbc
Eric Castronovo <ecastronovo@scu.edu>
2019-04-17 12:42:05 -0700

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Eric Castronovo on 4/17/19.
+//  Copyright © 2019 Eric Castronovo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
